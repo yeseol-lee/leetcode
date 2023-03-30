b8820d793c967af159bb25c5949d05da4f4d553b
1999onell <1999onell@gmail.com>
2023-03-30 20:27:29 +0800

    Find Numbers with Even Number of Digits-1

---
+++ Find Numbers with Even Number of Digits.js
@@ -0,0 +1,36 @@
+// [ -3 -1 0 2 3]
+// [ -3 -1 2 3 4]
+
+var findNumbers = function (nums) {
+  let result = [];
+  // 1. find index of 0
+  let startIndex = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= 0) {
+      startIndex = i;
+      return;
+    }
+  }
+
+  // 2. push to result 'value of square' is small one
+  let i = startIndex;
+  let j = startIndex - 1;
+  while (i < nums.length && j >= 0) {
+    const iValueSquare = nums[i] * nums[i];
+    const jValueSquare = nums[j] * nums[j];
+    if (iValueSquare < jValueSquare) {
+      result.push(iValueSquare);
+      i++;
+    } else if (jValueSquare < iValueSquare) {
+      result.push(jValueSquare);
+      j--;
+    } else {
+      result.push(iValueSquare);
+      result.push(jValueSquare);
+      i++;
+      j--;
+    }
+  }
+
+  // i, j 중 어느하나는 끝에 도달함.
+};
