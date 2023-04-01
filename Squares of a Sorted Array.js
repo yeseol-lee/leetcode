4d94469b5d109cb55dddd2549b902b551b035aea
1999onell <1999onell@gmail.com>
2023-04-01 19:08:01 +0800

    Suqares of a Sorted Array

--- Squares of a Sorted Array.js
+++ Squares of a Sorted Array.js
@@ -1,18 +1,25 @@
-// [ -3 -1 0 2 3]
-// [ -3 -1 2 3 4]
-
-var findNumbers = function (nums) {
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
   let result = [];
-  // 1. find index of 0
+  // 1. find index of 0 (define as startIndex)
   let startIndex = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] >= 0) {
-      startIndex = i;
-      return;
+
+  // check the array contains only minus value
+  if (nums[nums.length - 1] < 0) {
+    startIndex = nums.length;
+  } else {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] >= 0) {
+        startIndex = i;
+        break;
+      }
     }
   }
 
-  // 2. push to result 'value of square' is small one
+  // 2. push to result smaller 'value of square' value
   let i = startIndex;
   let j = startIndex - 1;
   while (i < nums.length && j >= 0) {
@@ -32,6 +39,7 @@ var findNumbers = function (nums) {
     }
   }
 
+  // 3. deal with left elements
   // i, j 중 어느하나는 끝에 도달함.
   // j가 끝에 달한 경우
   if (j < 0) {
@@ -40,5 +48,10 @@ var findNumbers = function (nums) {
     }
   } else {
     // i가 끝에 달한 경우
+    for (let k = j; k >= 0; k--) {
+      result.push(nums[k] * nums[k]);
+    }
   }
+
+  return result;
 };
