509fbd6ab93bef9b68bba39bd8bd792fd5a2d10c
1999onell <1999onell@gmail.com>
2023-04-05 21:13:15 +0800

    Merge Sorted Array -2

--- Merge Sorted Array.js
+++ Merge Sorted Array.js
@@ -10,27 +10,34 @@
 var merge = function (nums1, m, nums2, n) {
   let nums2Index = 0;
 
+  //         /
+  //   123 / 256;
+  //     /
+
   for (let nums1Index = 0; nums1Index < m; nums1Index++) {
     if (nums2[nums2Index] >= nums1[nums1Index]) {
-      console.log('nums1: ', nums1);
-      nums1Index++;
+      console.log('if nums1: ', nums1);
+      console.log('nums1Index: ', nums1Index);
+      console.log('nums2Index: ', nums2Index);
+
       continue;
     }
-    while (nums2[nums2Index] < nums1[nums1Index] || nums2Index < n) {
+    while (nums2[nums2Index] < nums1[nums1Index] && nums2Index < n) {
       // shift nums1 from nums1Index
+      console.log('while nums1: ', nums1);
+      console.log('nums1Index: ', nums1Index);
+      console.log('nums2Index: ', nums2Index);
+
       let savedValue = nums1[nums1Index];
       for (let i = nums1Index; i < m - 1; i++) {
         let temp = nums1[i + 1];
         nums1[i + 1] = savedValue;
         savedValue = temp;
       }
-
-      // put smaller nums2 value to empty nums1's place
-      nums1[nums1Index] = nums2[nums2Index];
-
-      nums1Index++;
-      nums2Index++;
     }
+    // put smaller nums2 value to empty nums1's place
+    nums1[nums1Index] = nums2[nums2Index];
+    nums2Index++;
   }
 
   return nums1;
