9f7c8aa0b3c2ce20bebebcf6699b9bf108e3fe82
1999onell <1999onell@gmail.com>
2023-04-13 05:08:52 +0800

    438 - 4

--- 438. Find All Anagrams in a String.js
+++ 438. Find All Anagrams in a String.js
@@ -9,32 +9,24 @@ var findAnagrams = function (s, p) {
   let pObject = {};
   let result = [];
 
-  // // make p to hashMap
-  // for (let i = 0; i < p.length; i++) {
-  //   pObject[p[i]] ? pObject[p[i]]++ : (pObject[p[i]] = 1);
-  // }
-
-  console.log('pObject: ', pObject);
+  // make p to hashMap
+  for (let i = 0; i < p.length; i++) {
+    pObject[p[i]] ? pObject[p[i]]++ : (pObject[p[i]] = 1);
+  }
 
   for (let i = 0; i < s.length - p.length + 1; i++) {
-    let pObject = {};
-    // make p to hashMap
-    for (let j = 0; j < p.length; j++) {
-      pObject[p[j]] ? pObject[p[j]]++ : (pObject[p[j]] = 1);
-    }
-
-    console.log('i: ', i);
-    console.log('pObject: ', pObject);
+    let pObjectCopy = { ...pObject };
 
     let k = i;
     let check = true;
+    let nonExistingLetter = false;
     while (k < i + p.length && check) {
-      console.log('hello k: ', k, 'i: ', i);
-      //   console.log('pObject: ', pObject);
-      if (pObject[s[k]]) {
-        console.log('o');
-        pObject[s[k]]--;
+      if (pObjectCopy[s[k]]) {
+        pObjectCopy[s[k]]--;
+      } else if (pObjectCopy[s[k]] === 0) {
+        check = false;
       } else {
+        nonExistingLetter = true;
         check = false;
       }
       k++;
@@ -42,7 +34,6 @@ var findAnagrams = function (s, p) {
 
     // anagram인 경우
     if (check) {
-      console.log('check true');
       result.push(i);
 
       // sliding window로 다음요소가 anagram인지 확인
@@ -52,44 +43,11 @@ var findAnagrams = function (s, p) {
       }
       i++;
     }
-  }
 
+    // p에 존재하지 않는 letter가 있는 경우 i를 그 다음 index로 이동
+    if (nonExistingLetter) {
+      i = k - 1;
+    }
+  }
   return result;
 };
-
-console.log(findAnagrams('cbaebabacd', 'abc'));
-
-// // Hash Table, String, Sliding Window
-// /**
-//  * @param {string} s
-//  * @param {string} p
-//  * @return {number[]}
-//  */
-// var findAnagrams = function (s, p) {
-//   let result = [];
-
-//   for (let i = 0; i < s.length - p.length + 1; i++) {
-//     let pObject = {};
-//     // make p to hashMap
-//     for (let j = 0; j < p.length; j++) {
-//       pObject[p[j]] ? pObject[p[j]]++ : (pObject[p[j]] = 1);
-//     }
-
-//     let k = i;
-//     let check = true;
-//     while (k < i + p.length && check) {
-//       if (pObject[s[k]]) {
-//         pObject[s[k]]--;
-//       } else {
-//         check = false;
-//       }
-//       k++;
-//     }
-
-//     if (check) {
-//       result.push(i);
-//     }
-//   }
-
-//   return result;
-// };
