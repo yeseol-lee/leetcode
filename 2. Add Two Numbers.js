493247f36abd22464c51fc93f88a1c07fafc0752
1999onell <1999onell@gmail.com>
2023-04-14 20:33:03 +0800

    2 - 2

--- 2. Add Two Numbers.js
+++ 2. Add Two Numbers.js
@@ -11,7 +11,18 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
+  let result = [];
+  // 각 자리의 합이 10이 넘으면 true로 바꿔줌
   let add10 = false;
 
-  do {} while (l1.next);
+  do {
+    let sum = add10 ? 1 + l1.val + l2.val : l1.val + l2.val;
+    if (sum >= 10) {
+      sum -= 10;
+      add10 = true;
+    } else {
+      add10 = false;
+    }
+    result.push(sum);
+  } while (l1.next && l2.next);
 };
