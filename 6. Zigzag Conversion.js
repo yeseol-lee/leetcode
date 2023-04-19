91723de7736bf35c742cd289833fc7dc0dbe4033
1999onell <1999onell@gmail.com>
2023-04-19 18:51:27 +0800

    6 - 1

---
+++ 6. Zigzag Conversion.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  // 1. numRows 만큼 빈 array를 만듦
+  // 2. 숫자 위치 index -> numRows에서 시작, ex) 4 3 2 1 2 3 4 .. 로 카운트 하기
+  // 3. 홀수번째 인 경우 모든 array에 넣기, 짝수번째 인 경우 위 index 참고
+};
