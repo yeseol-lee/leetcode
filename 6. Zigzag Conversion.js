7fcb3090703fa3b5acef2d2af0c1a126681ee680
1999onell <1999onell@gmail.com>
2023-04-20 22:16:38 +0800

    6 - 2

--- 6. Zigzag Conversion.js
+++ 6. Zigzag Conversion.js
@@ -5,6 +5,11 @@
  */
 var convert = function (s, numRows) {
   // 1. numRows 만큼 빈 array를 만듦
+  // 변수를 동적으로 생성 -> eval?
+  // 이것 말고 방법이 없을까?
+  for (let i = 0; i < numRows.length; i++) {
+    let;
+  }
   // 2. 숫자 위치 index -> numRows에서 시작, ex) 4 3 2 1 2 3 4 .. 로 카운트 하기
   // 3. 홀수번째 인 경우 모든 array에 넣기, 짝수번째 인 경우 위 index 참고
 };
